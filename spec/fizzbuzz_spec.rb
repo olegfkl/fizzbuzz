8163be1cb2c7cbd7c0f829219c866fe83a13b4a0
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-18 13:58:46 +0100

    Adds first test and and updates fizzbuzz.rb to pass it

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,3 +2,8 @@
 # require './lib/fizzbuzz'
 # require_relative '../lib/fizzbuzz'
 require 'fizzbuzz'
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+end
