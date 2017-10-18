9d23b811d180fbb3c3ec46516ba835fe2796fe51
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-18 14:47:21 +0100

    Adds test when number is multiple of both 3 and 5. Updates fizzbuzz.rb to pass it

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
-   if number % 3 == 0
+  if  number % 3 == 0 && number % 5 == 0
+    'Fizzbuzz'
+  elsif number % 3 == 0
      'Fizz'
    elsif number % 5 == 0
      'Buzz'
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,7 @@
 # require './lib/fizzbuzz'
 # require_relative '../lib/fizzbuzz'
 require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3 or multiple of 3' do
     expect(fizzbuzz(3)).to eq 'Fizz'
@@ -15,5 +16,13 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'Buzz'
     expect(fizzbuzz(25)).to eq 'Buzz'
     expect(fizzbuzz(100)).to eq 'Buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "Fizzbuzz" when passed a number that is, both, multiple of 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'Fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'Fizzbuzz'
   end
 end
