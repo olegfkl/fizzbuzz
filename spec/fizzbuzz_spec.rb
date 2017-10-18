270dd22a5fefdb0d156eec05e41c62b0e5d845a4
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-18 14:24:26 +0100

    Updates first test to test for multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,7 +3,9 @@
 # require_relative '../lib/fizzbuzz'
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed 3 or multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
+    expect(fizzbuzz(36)).to eq 'fizz'
   end
 end
